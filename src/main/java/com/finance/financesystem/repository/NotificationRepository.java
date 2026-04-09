1246f5def1e35086a387f30bd8c3e98076958dac
Damaris <ortizmonicadamaris@gmail.com>
2026-04-09 00:56:54 -0600

    Se sube transactionRequest

--- src/main/java/com/finance/financesystem/repository/NotificationRepository.java
+++ src/main/java/com/finance/financesystem/repository/NotificationRepository.java
@@ -1,9 +1,11 @@
 package com.finance.financesystem.repository;
 
+import com.finance.financesystem.dto.transactionrequest.TransactionRequest;
 import com.finance.financesystem.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    Notification save(TransactionRequest transactionRequest);
 }
--- src/main/java/com/finance/financesystem/service/TransactionService.java
+++ src/main/java/com/finance/financesystem/service/TransactionService.java
@@ -11,4 +11,5 @@ public interface TransactionService {
 
     String addTransaction(TransactionRequest transactionRequest);
 
+
 }
